c40f20c4ca08e58a9283aa8e8375c291c8f95805
MarcinWorkDev <marcin93li@gmail.com>
2019-11-27 11:30:43 +0100

    Update NamedAttribute.cs

--- Library/NamedAttribute.cs
+++ Library/NamedAttribute.cs
@@ -5,6 +5,11 @@ namespace MWork.Extensions.Microsoft.DependencyInjection
     [AttributeUsage(AttributeTargets.Constructor)]
     public class NamedAttribute : Attribute
     {
+        public string Name { get; }
         
+        public NamedAttribute(string name)
+        {
+            Name = name;
+        }
     }
 }
